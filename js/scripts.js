9bcbef78805ebf5f39b66952a262f0e3e2619f03
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-08-19 23:07:22 -0700

    moving initial files

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// var myName = 'Bob';
+// document.write(myName);
+// myName = 'John Doe';
+// document.write(myName);
+
+// var simpleAddition = 2 + 2;
+// document.write(simpleAddition);
+
+// var size = 100;
+// var doubleSize = size * 2;
+// var minSize = (doubleSize * 2) - (size / 2);
+// document.write(minSize);
+
+alert("hello world");
+
+var favoriteFood = 'chipotle';
+document.write(favoriteFood + " is my favorite food");
