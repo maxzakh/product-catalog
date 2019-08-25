9b0c421046fd2c9e208613b59b523c939618929a
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-08-24 22:53:49 -0700

    1.4

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,21 @@ var repository = [
     }
 ];
 
-for (let i = 0; i <= repository.length; i++) {
-    document.write(repository[i].name + " ");
-    document.write("\(height: " + repository[i].height + "\) ");
-    if (repository[i].height < 2) {
-      document.write("- It's so small ");
-    }
-  document.write("<br>");
-}
+var repository2 = {
+  name: 'Snorlax',
+  height: 2.1,
+  types: ['normal']
+};
+
+// for (let i = 0; i <= repository.length; i++) {
+//     document.write(repository[i].name + " ");
+//     document.write("\(height: " + repository[i].height + "\) ");
+//     if (repository[i].height < 2) {
+//       document.write("- It's so small ");
+//     }
+//   document.write("<br>");
+// }
+
+Object.keys(repository2).forEach(function(property) {
+  document.write([property] + ': ' + repository2[property] + '<br>');
+});
