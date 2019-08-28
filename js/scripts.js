e3b7ffa27d03d00c7fd9f82c088ae1fdac9de2dd
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-08-28 11:24:27 -0700

    implementing functional programming

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,63 @@
-var repository = [
-    {
-        name: 'Snorlax',
-        height: 2.1,
-        types: ['normal']
-    },
-    {
-        name: 'Electabuzz',
-        height: 1.1,
-        types: ['electric']
-    },
-    {
-        name: 'Slowbro',
-        height: 2.1,
-        types: ['psychic', 'water']
-    }
-];
-
-var repository2 = {
-  name: 'Snorlax',
-  height: 2.1,
-  types: ['normal']
-};
-
-// for (let i = 0; i <= repository.length; i++) {
-//     document.write(repository[i].name + " ");
-//     document.write("\(height: " + repository[i].height + "\) ");
-//     if (repository[i].height < 2) {
-//       document.write("- It's so small ");
+// var repository = [
+//     {
+//         name: 'Snorlax',
+//         height: 2.1,
+//         types: ['normal']
+//     },
+//     {
+//         name: 'Electabuzz',
+//         height: 1.1,
+//         types: ['electric']
+//     },
+//     {
+//         name: 'Slowbro',
+//         height: 2.1,
+//         types: ['psychic', 'water']
 //     }
-//   document.write("<br>");
-// }
+// ];
 
-repository.forEach((pokemon) => {
-  Object.keys(pokemon).forEach(function(property) {
-    if (property === "name") {
-      document.write(`${pokemon[property]}<br>`);
-    } else {
-      document.write(`${property}: ${pokemon[property]} <br>`);
+// var repository2 = {
+//   name: 'Snorlax',
+//   height: 2.1,
+//   types: ['normal']
+// };
+
+// // for (let i = 0; i <= repository.length; i++) {
+// //     document.write(repository[i].name + " ");
+// //     document.write("\(height: " + repository[i].height + "\) ");
+// //     if (repository[i].height < 2) {
+// //       document.write("- It's so small ");
+// //     }
+// //   document.write("<br>");
+// // }
+
+// repository.forEach((pokemon) => {
+//   Object.keys(pokemon).forEach(function(property) {
+//     if (property === "name") {
+//       document.write(`${pokemon[property]}<br>`);
+//     } else {
+//       document.write(`${property}: ${pokemon[property]} <br>`);
+//     }
+//   });
+// });
+
+var pokemonRepository = (function () {
+  var repository = []; // empty array
+
+  return {
+    add: function(pokemon) {
+      repository.push(pokemon);
+    },
+    getAll: function() {
+      return repository;
     }
-  });
-});
+  };
+})();
 
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: 'Pikachu' });
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+pokemonRepository.forEach(pokemon => {
+  console.log(pokemon);
+});
