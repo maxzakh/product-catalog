b48533d18ff1ec228b20d5475770574d822a02d1
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-09-06 23:22:05 -0700

    Added ability to display new objects in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -17,50 +17,62 @@
 
 var $pokeList = document.querySelector('.pokeList');
 
+const pokemonRepository = (function () {
+  const repository = [
+    {
+      name: 'Snorlax',
+      height: 2.1,
+      types: ['normal'],
+    },
+    {
+      name: 'Electabuzz',
+      height: 1.1,
+      types: ['electric'],
+    },
+    {
+      name: 'Slowbro',
+      height: 2.1,
+      types: ['psychic', 'water'],
+    },
+  ];
 
-
-var pokemonRepository = (function () {
-  var repository = [{
-            name: 'Snorlax',
-            height: 2.1,
-            types: ['normal']
-        },
-        {
-            name: 'Electabuzz',
-            height: 1.1,
-            types: ['electric']
-        },
-        {
-            name: 'Slowbro',
-            height: 2.1,
-            types: ['psychic', 'water']
-        }]; // empty array
-
-    var $pokeList = document.querySelector('.pokeList');
-  
-    repository.forEach((pokemon) => {
-        var button = document.createElement('button');
-        button.classList.add('pokeDex');
-        button.innerHTML = pokemon.name;
-    
-        var listItem = document.createElement('li');
-        $pokeList.appendChild(listItem);
-        listItem.appendChild(button);
-    });
-  
   return {
-    add: function(pokemon) {
+    add(pokemon) {
       repository.push(pokemon);
     },
-    getAll: function() {
-      return repository;
-    }
-  };
-})();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll());
+    // insert addListItem method here
+    addListItem(pokemon) {
+      var button = document.createElement('button');
+      button.classList.add('pokeDex');
+      button.innerHTML = pokemon.name;
+        
+      var listItem = document.createElement('li');
+      $pokeList.appendChild(listItem);
+      listItem.appendChild(button);
+    },
+    // insert showDetails method here
+
+    getAll() {
+      return repository;
+    },
+  };
+}());
+
+pokemonRepository.getAll().forEach((pokemon) => {
+  var button = document.createElement('button');
+  button.classList.add('pokeDex');
+  button.innerHTML = pokemon.name;
+
+  var listItem = document.createElement('li');
+  $pokeList.appendChild(listItem);
+  listItem.appendChild(button);
+});
+
+console.log(pokemonRepository.add({ name: 'Pikachu' })); // []
+console.log(pokemonRepository.addListItem({ name: 'Pikachu' }));
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
 
 // repository.forEach((pokemon) => {
 //     var listItem = document.createElement('li');
