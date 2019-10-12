9bbc433952b1cefa69a3e40a2a3bc211aaa5347c
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2019-10-12 00:10:27 -0700

    add modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,115 @@ var pokemonRepository = (function () {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  var $modalContainer = document.querySelector('#modal-container');
+  var dialogPromiseReject; // This can be set later, by showDialog
+
+  function showModal(title, text) {
+    // Clear all existing modal content
+    $modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    var titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    var contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    $modalContainer.appendChild(modal);
+
+    $modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  function showDialog(title, text) {
+    showModal(title, text);
+
+    // We want to add a confirm and cancel button to the modal
+    var modal = $modalContainer.querySelector('.modal');
+
+    var confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    var cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    // We want to focus the confirmButton so that the user can simply press Enter
+    confirmButton.focus();
+
+    return new Promise((resolve, reject) => {
+      cancelButton.addEventListener('click', hideModal);
+      confirmButton.addEventListener('click', () => {
+        dialogPromiseReject = null; // Reset this
+        hideModal();
+        resolve();
+      });
+
+      // This can be used to reject from other functions
+      dialogPromiseReject = reject;
+
+      confirmButton.addEventListener('click', () => {
+        hideModal();
+        resolve();
+      })
+    });
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('hello', 'This is the modal content!');
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  $modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  document.querySelector('#show-dialog').addEventListener('click', () => {
+    showDialog('Confirm action', 'Are you sure you want to do this?');
+  });
+
+  document.querySelector('#show-dialog').addEventListener('click', () => {
+    showDialog('Confirm action', 'Are you sure you want to do this?').then(function () {
+      alert('confirmed!');
+    }, () => {
+      alert('not confirmed');
+    });
+  });
+
   // Other functions remain here
   function add(pokemon) {
     repository.push(pokemon);
@@ -26,6 +135,7 @@ var pokemonRepository = (function () {
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      pokemonRepository.showModal(pokemon.name, pokemon.imageUrl);
     });
   };
 
@@ -70,7 +180,9 @@ var pokemonRepository = (function () {
     getAll: getAll,
     // search: search,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showModal,
+    hideModal
   };
 })();
 
